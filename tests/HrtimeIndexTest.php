3e6bc2c959099f587618030d40d5e7c86d13a050
Julien Neuhart <neuhart.julien@gmail.com>
2023-12-06 11:29:38 +0100

    chore: remove deprecated stuff

---
+++ tests/HrtimeIndexTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use Gotenberg\HrtimeIndex;
+
+it(
+    'creates alphabetical ordered indexes',
+    function (): void {
+        $index   = new HrtimeIndex();
+        $indexes = [];
+
+        for ($i = 0; $i < 100; $i++) {
+            $indexes[$i] = $index->create();
+
+            if ($i === 0) {
+                continue;
+            }
+
+            $result = $indexes[$i] > $indexes[$i - 1];
+            expect($result)->toBeTrue();
+        }
+    }
+);
