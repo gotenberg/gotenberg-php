19b2136181f7ee236e4f646ab5f572903b68454e
Yan Buatois <yan.buatois@mediapart.fr>
2024-04-11 16:42:00 +0200

    fix: only serialize defined cookie parameters

--- src/Modules/ChromiumCookie.php
+++ src/Modules/ChromiumCookie.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Gotenberg\Modules;
 
-class ChromiumCookie
+use JsonSerializable;
+
+class ChromiumCookie implements JsonSerializable
 {
     public function __construct(
         public readonly string $name,
@@ -15,5 +17,33 @@ class ChromiumCookie
         public readonly bool|null $httpOnly = null,
         public readonly string|null $sameSite = null,
     ) {
+    }
+
+    /** @return array<string, string|bool> */
+    public function jsonSerialize(): array
+    {
+        $serialized = [
+            'name' => $this->name,
+            'value' => $this->value,
+            'domain' => $this->domain,
+        ];
+
+        if ($this->path !== null) {
+            $serialized['path'] = $this->path;
+        }
+
+        if ($this->secure !== null) {
+            $serialized['secure'] = $this->secure;
+        }
+
+        if ($this->httpOnly !== null) {
+            $serialized['httpOnly'] = $this->httpOnly;
+        }
+
+        if ($this->sameSite !== null) {
+            $serialized['sameSite'] = $this->sameSite;
+        }
+
+        return $serialized;
     }
 }
